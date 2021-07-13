a5647702250409000e8a0df62dbeafdde774a6c9
Miraz Hossain <miraz4300@gmail.com>
2021-07-13 15:26:36 +0600

    TRN Implemented

--- routes/core.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/v1/profile/:platform/:gamertag', (req, res) => {
-    console.log(req.params.platform, req.params.gamertag);
-    res.send('Hello');
-});
-
-module.exports = router;
