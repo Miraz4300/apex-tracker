80d4ffbb8e6a56b613be29ada61b3e1ca8b41dd3
Miraz Hossain <miraz4300@gmail.com>
2021-07-13 18:47:24 +0600

    frontend client server v0.1

---
+++ client/src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Search from './components/Search'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: "/",
+        name: "search",
+        component: Search,
+      }
+    ]
+});
