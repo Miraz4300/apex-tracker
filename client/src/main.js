f9c7b8587e942af1c31ec50d7aa7892739bfdb49
Miraz Hossain <miraz4300@gmail.com>
2021-07-14 16:49:51 +0600

    updated main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueToasted from 'vue-toasted'
-
-VueToasted.use(VueToasted,{
-    iconPack: 'fontawesome'
-});
 
 createApp(App).use(router).mount('#app')
