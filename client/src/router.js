836bfc70ec960bda31f3611e5861d9b22e099ef8
Miraz Hossain <miraz4300@gmail.com>
2021-07-14 16:45:12 +0600

    profile route integration

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Search from './components/Search'
+import Profile from './components/Profile'
 
 const routes = [
   {
     path: "/",
     name: "search",
-    component: Search,
+    component: Search
+  },
+  {
+    path: "/profile/:platform/:gamertag",
+    name: "profile",
+    component: Profile
   },
 ];
 
