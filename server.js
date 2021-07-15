f76a6385573123dd6a4e208a3e314a7ae54fd507
Miraz Hossain <miraz4300@gmail.com>
2021-07-16 03:59:32 +0600

    removed prod handling that cause crashing app

--- server.js
+++ server.js
@@ -4,21 +4,15 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const app = express(); 
+
 app.use('/api/v1/profile', require('./routes/profile'));
 
 // Dev logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
-
-// Production Hangdling
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/public/'));
-  
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-  }
-
-const app = express();  
+ 
 const port = process.env.PORT || 8800;
 
 app.listen(port, () => {
